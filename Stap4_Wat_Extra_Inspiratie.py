97cf7ef1bab2f5d045404e621cad44e2d5715bff
Maurits Groen <r1037355@student.thomasmore.be>
2025-12-15 10:28:33 +0100

    Belevingsdag AI

--- Stap4_Wat_Extra_Inspiratie.py
+++ Stap4_Wat_Extra_Inspiratie.py
@@ -1,22 +1,21 @@
 # Imports
 import requests
 from bs4 import BeautifulSoup
-import openai
+from openai import OpenAI
 from fpdf import FPDF
 from gtts import gTTS
-from sys import exit
 
 # OpenAI client instellen
-client = openai.OpenAI(
-    api_key="123123",
-    base_url="https://api.algion.dev/v1"
+client = OpenAI(
+    api_key="ZET_HIER_JE_GROQ_API_SLEUTEL",
+    base_url="https://api.groq.com/openai/v1"
 )
 
 # URL van het nieuws
-URL = "https://www.techtarget.com/searchapparchitecture/definition/object-oriented-programming-OOP"
+url = "https://www.techtarget.com/searchapparchitecture/definition/object-oriented-programming-OOP"
 
 # Haal website op
-response = requests.get(URL)
+response = requests.get(url)
 if response.status_code != 200:
     print("Kan geen informatie ophalen van de website!")
     exit()
@@ -25,20 +24,16 @@ if response.status_code != 200:
 soup = BeautifulSoup(response.content, 'html.parser')
 data = soup.text
 
-# laat AI een samenvatting maken
-prompt = """Maak een samenvatting van ongeveer 3 minuten leeswerk. 
-            Ik wil de samenvatting in het Nederlands ontvangen. 
-            Ik wil enkel de samenvatting en geen extra informatie,
-            puur een samenvatting teruggeven : """ + data
+#laat AI een samenvatting maken
+prompt = "Maak een samenvatting van ongeveer 3 minuten leeswerk. Ik wil de samenvatting in het Nederlands ontvangen. Ik wil enkel de samenvatting en geen extra informatie puur samenvatting teruggeven : " + data
+response = client.responses.create(
+            model="openai/gpt-oss-20b",
+            input=prompt
+        )
 
-response = client.chat.completions.create(
-    model="gpt-4",
-    messages=[
-        {"role": "user", "content": prompt}
-    ]
-)
+samenvatting = response.output_text
 
-samenvatting = response.choices[0].message.content
+# Print samenvatting
 print("Samenvatting gemaakt")
 
 # Maak PDF
@@ -46,13 +41,11 @@ pdf = FPDF()
 pdf.add_page()
 pdf.set_auto_page_break(auto=True, margin=15)
 pdf.set_font("Arial", size=12)
-pdf.multi_cell(0, 10, samenvatting.encode(
-    'latin-1', 'replace').decode('latin-1'))
+pdf.multi_cell(0, 10, samenvatting.encode('latin-1', 'replace').decode('latin-1'))
 pdf.output("samenvatting_nieuws.pdf")
 print("PDF opgeslagen als samenvatting_nieuws.pdf")
 
-# --- Audio genereren met Google tekst to speech ---
-# kan veranderd worden naar elke taal die je wilt
+# --- Audio genereren (gewoon tekst to speech, dus letterlijk overnemen) ---
 tts = gTTS(samenvatting, lang="nl")
 tts.save("samenvatting_nieuws.mp3")
 print("Audio opgeslagen als samenvatting_nieuws.mp3")
