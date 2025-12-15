03de4c0f587ca2e80d590a2a21153b683cf79330
Maurits Groen <r1037355@student.thomasmore.be>
2025-12-15 10:19:49 +0100

    Update Stap1_Maak_Je_Eigen_Chatbot.py

--- Stap1_Maak_Je_Eigen_Chatbot.py
+++ Stap1_Maak_Je_Eigen_Chatbot.py
@@ -1,22 +1,20 @@
-# Imports
-import openai
+#Imports
+from openai import OpenAI
 
 # API credentials
-client = openai.OpenAI(
-    api_key="123123",
-    base_url="https://api.algion.dev/v1"
+client = OpenAI(
+    api_key="ZET_HIER_JE_GROQ_API_SLEUTEL",
+    base_url="https://api.groq.com/openai/v1"
 )
 
 # Vraag de gebruiker om een vraag
 vraag = input("Wat wil je weten? ")
 
-# Stuur HTTP verzoek naar API om antwoord te genereren door AI
-response = client.chat.completions.create(
-    model="gpt-4o",
-    messages=[
-        {"role": "user", "content": vraag}
-    ]
-)
+#Stuur HTTP verzoek naar API om antwoord te genereren door AI
+response = client.responses.create(
+            model="openai/gpt-oss-20b",
+            input=vraag,
+        )
 
-# Geef het antwoord van API (de AI chatbot) weer
-print(response.choices[0].message.content)
+#Geef het antwoord van API (de AI chatbot) weer
+print(response.output_text)
